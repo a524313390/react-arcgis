2d2aee6fddb2bbdb7b9abfe9f15ee243f034bab8
undefined <30929792+a524313390@users.noreply.github.com>
2021-10-27 21:40:41 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import RouterBox from '@/router';
 import "@arcgis/core/assets/esri/themes/light/main.css"
 import '@/assets/index.less'
 import 'antd/dist/antd.css';
+console.log(12312)
 ReactDOM.render(
     <React.StrictMode>
         <RouterBox />
